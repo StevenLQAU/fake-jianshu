70a09a05c193376718d41c9ef82677ec415db353
Steven Lu <stevenl.medicaldirector@outlook.com>
2018-08-09 22:57:06 +1000

    Added redux-thunk to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
-import { compose, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk'
 import reducer from './reducer'
 
 // const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION__() || compose;
 
 // export const store = createStore(reducer, composeEnhancers());
 export const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 ));
